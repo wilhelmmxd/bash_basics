4e373fd298627c1e6036a032382dab111b6be78f
William Stein <thewillstein@gmail.com>
2025-03-29 17:12:05 -0400

    Parallel life

--- 04-life.sh
+++ 04-life.sh
@@ -9,6 +9,15 @@ else
    echo "Awww... You don't know the meaning of life"
 fi
 
+echo "Is it morning where you are?"
+read morning
+
+if ["$morning" -eq YES]; then
+   echo "WOW, it is morning!"
+else
+   echo "Hope you sleep well!"
+fi
+
 #  here are some other arithemetic comparison operators
 # -eq -ne -gt -ge -lt -le
 
